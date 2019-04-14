2e30d91339c07e8dc69d711b9d783a85b9b58834
Nadim Mostafa <nadim24h@gmail.com>
2019-04-14 08:44:02 +0600

    Added a line in the branchB

--- hello.c
+++ hello.c
@@ -5,7 +5,10 @@ int main()
     printf("Start my github learning");
 
     printf("push my code on github succesfully");
-    printf("we are in the master branch");
+    //printf("we are in the master branch");
+
+    printf("added a line in the branchB");
+
 
     return 0;
 }
