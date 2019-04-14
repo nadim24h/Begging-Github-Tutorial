6e5e589e1385d0e489a753f3b3369b68979a778a
Nadim Mostafa <nadim24h@gmail.com>
2019-04-14 08:26:17 +0600

    Change command

--- hello.c
+++ hello.c
@@ -2,9 +2,15 @@
 
 int main()
 {
+    int a;
     printf("Start my github learning");
 
-    printf("push my code on github succesfully");
+    //printf("push my code on github succesfully");
+    printf("what is git?");
+
+    scanf("%d", &a);
+
+    printf("%d", a);
 
 
     return 0;
